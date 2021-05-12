7eef4141d11c3bb7dcde0215eb7491b2fe072f55
Yaroslav <yaroslav.pikiner@chiws.com>
2021-05-12 15:04:42 +0300

    add form and form validation

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-  Link,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+    Link,
 } from "react-router-dom";
 
 import ProtectedRoute from "./ProtectedRoute";
@@ -13,44 +13,38 @@ import Login from "./screens/login";
 import Header from './components/header'
 
 function Routes() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const login = () => {
-    setIsAuthenticated(true);
-  };
+    const login = () => {
+        setIsAuthenticated(true);
+    };
 
-  const logout = () => {
-    setIsAuthenticated(false);
-  };
+    const logout = () => {
+        setIsAuthenticated(false);
+    };
+    const AuthContext = React.createContext(isAuthenticated);
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/" exact>
+                        {isAuthenticated ?
+                            <Redirect to="/" />
+                            : <Redirect to="/login" />}
+                    </Route>
+                    <ProtectedRoute
+                        isAuthenticated={isAuthenticated}
+                        path="/"
+                        logout={logout}
+                        component={Header}
+                    />
+                    {/* <Route path="/user/:id" component={login}/> */}
 
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            {isAuthenticated ? (
-              <Redirect to="/login" />
-            ) : (
-              <div>
-                <h1>Homepage</h1>
-                <Link to="/login">Go to secret</Link>
-                <button onClick={login}>Log in</button>
-              </div>
-            )}
-          </Route>
-          <ProtectedRoute
-            isAuthenticated={isAuthenticated}
-            path="/"
-            logout={logout}
-            component={Header}
-          />
-          <Route path="/login" component={login}/>
-          {/* <Route path="/user/:id" component={login}/> */}
-          
-        </Switch>
-      </Router>
-    </div>
-  );
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default Routes;
--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,5 +1,85 @@
-function Login() {
-    return <h1>Hello epta its login</h1>
-}
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box'
+
+const validationSchema = yup.object({
+    email: yup
+        .string('Enter your email')
+        .email('Enter a valid email')
+        .required('Email is required'),
+    password: yup
+        .string('Enter your password')
+        .min(8, 'Password should be of minimum 8 characters length')
+        .required('Password is required'),
+});
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+
+}));
+
+const Login = () => {
+    const classes = useStyles();
+    const formik = useFormik({
+        initialValues: {
+            email: 'foobar@example.com',
+            password: 'adminadmin',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <div className={classes.paper}>
+            <form className={classes.root} onSubmit={formik.handleSubmit}>
+                <TextField
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
+                <TextField
+                    fullWidth
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                />
+                <Button type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}>
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
 
 export default Login;
