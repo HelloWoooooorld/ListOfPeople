7bea70bd978ebe1502d575997895928e0d961a23
Yaroslav <yaroslav.pikiner@chiws.com>
2021-05-13 16:44:47 +0300

    add post rquest

--- src/pages/TableId/index.js
+++ src/pages/TableId/index.js
@@ -1,22 +1,10 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import { useFormik } from 'formik';
-import * as yup from 'yup';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
-
-const validationSchema = yup.object({
-    email: yup
-        .string('Enter your email')
-        .email('Enter a valid email')
-        .required('Email is required'),
-    password: yup
-        .string('Enter your password')
-        .min(8, 'Password should be of minimum 8 characters length')
-        .required('Password is required'),
-});
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -57,38 +45,52 @@ const TableId = () => {
     const loc = useLocation();
     const [userId, setUserId] = useState([])
 
-    const formik = useFormik({
-        initialValues: {
-            email: 'foobar@example.com',
-            password: 'adminadmin',
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            history.push("/table");
-            console.log(JSON.stringify(values, null, 2));
-        },
-    });
-
-
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/users/${loc.pathname.slice(7)}`)
             .then((response) => response.json())
             .then((data) => setUserId(data))
             .catch((error) => console.log(error.message));
     }, []);
+    const formik = useFormik({
+        initialValues: {
+            id: userId.id,
+            name: userId.name,
+            username: userId.username,
+            email: userId.email,
+            phone: userId.phone,
+            website: userId.website
+        },
+        onSubmit: async (values) => {
+            await saveData(values);
+        },
+    });
 
-    const {company, address, ...users} = userId
-    
+    const saveData = async (val) => {
+        await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+            method: 'POST',
+            body: JSON.stringify(val),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
+
+    const { company, address, ...users } = userId
+
     const result = Object.entries(users).map((item, i) => {
         return <TextField
             className={classes.input}
             key={i++}
             id={item[0]}
+            name={item[0]}
             label={item[0]}
             defaultValue={item[1]}
             InputProps={{
                 readOnly: false,
-            }} />
+            }}
+            onChange={formik.handleChange} />
     })
 
     return (
