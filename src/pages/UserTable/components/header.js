31086b868546ecddecbdb227c9ab65fd50c4eb85
Yaroslav <yaroslav.pikiner@chiws.com>
2021-05-14 10:31:56 +0300

    divided into chunks user Table

---
+++ src/pages/UserTable/components/header.js
@@ -0,0 +1,30 @@
+import TableCell from '@material-ui/core/TableCell';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    cell: {
+        cursor: 'pointer',
+    },
+});
+
+const HeaderTitle = ({ sortedUsers, isFiltered }) => {
+    const classes = useStyles();
+    const cellName = ['Id', 'Name', 'User name', 'Email', 'Phone', 'Website']
+
+    return (
+        cellName.map(item =>
+            <TableCell 
+            className={classes.cell} 
+            key={item} align="right" 
+            onClick={() => sortedUsers()}> 
+            {isFiltered ? <>{item} <ArrowUpwardIcon /> </> 
+            : <>{item} <ArrowDownwardIcon /> </>} 
+            </TableCell>
+        )
+    )
+}
+
+export default HeaderTitle;
--- src/pages/UserTable/index.js
+++ src/pages/UserTable/index.js
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { useHistory } from "react-router-dom";
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-
+import TableRow from '@material-ui/core/TableRow';
+import HeaderTitle from './components/header';
+import TableData from './components/tableData';
 
 const StyledTableRow = withStyles((theme) => ({
     root: {
@@ -30,9 +28,10 @@ const UserTable = () => {
     const classes = useStyles();
     let history = useHistory();
 
+
     const [users, setUsers] = useState([]);
-    const [isFiltered, setIsFiltered] = useState(false)
-    const cellName = ['Id', 'Name', 'User name', 'Email', 'Phone', 'Website']
+    const [isFiltered, setIsFiltered] = useState(false);
+    const [sortConfig, setSortConfig] = useState(null);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -48,37 +47,36 @@ const UserTable = () => {
     const handleClick = (id) => history.push(`table/${id}`);
 
     const rows = users.map(item => createData(item.id, item.name, item.username, item.email, item.phone, item.website))
-    console.log(rows);
 
-    
+    const toogle = useCallback(() => {
+        setIsFiltered(v => !v);
+    }, [])
+
     const sortedUsers = () => {
-        setIsFiltered(!false)
-        return rows.sort((a, b) => {
-            return b.id - a.id
-        })
+        rows.sort((a, b) => {
+            if (a.id < b.id) {
+                toogle();
+                return sortConfig === 'ascending' ? -1 : 1;
+            }
+            if (a.id > b.id) {
+                toogle();
+                return sortConfig === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
     }
 
-    const tableTitle = cellName.map(item => <TableCell key={item} align="right" onClick={() => sortedUsers()}> {isFiltered ?  <ArrowDownwardIcon/> :  <ArrowUpwardIcon/>} </TableCell>)
 
     return (
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="customized table">
                 <TableHead>
                     <StyledTableRow>
-                        {tableTitle}
+                        <HeaderTitle sortedUsers={sortedUsers} isFiltered={isFiltered} />
                     </StyledTableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow key={row.id} onClick={() => handleClick(row.id)}>
-                            <TableCell align="right">{row.id}</TableCell>
-                            <TableCell align="right">{row.name}</TableCell>
-                            <TableCell align="right">{row.username}</TableCell>
-                            <TableCell align="right">{row.email}</TableCell>
-                            <TableCell align="right">{row.phone}</TableCell>
-                            <TableCell align="right">{row.website}</TableCell>
-                        </StyledTableRow>
-                    ))}
+                    <TableData rows={rows} handleClick={handleClick} />
                 </TableBody>
             </Table>
         </TableContainer>
